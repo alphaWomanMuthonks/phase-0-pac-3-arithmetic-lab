613b1a7147379c001d44fd304eb973e56c4248cc
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-08-14 22:45:15 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,39 @@
+number= 10;
+function add5() {
+    return (number +=5);
+}
+function subtract1() {
+    return (number -=1);
+}
+function multiplyBy3() {
+    return (number *=3);
+}
+function divideBy4() {
+    return (number /=4);
+}
+function add(a,b) {
+    return (a+b);
+}
+function subtract(a,b) {
+    return (a-b);
+}
+function multiply(a,b) {
+    return (a*b);
+}
+function divide(a,b) {
+    return (a/b);
+}
+function increment(n) {
+    return (n+=1);
+}
+function decrement(n) { 
+    return (n-=1);
+}
+function makeInt(n) {
+    return parseInt(n,10);
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+
+
